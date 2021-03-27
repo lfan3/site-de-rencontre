09cf7d84448bb7d974724703505cdc7f4f3787b4
lfan3 <fifidemacici@gmail.com>
2021-03-27 22:51:52 +0100

    update all in app

--- Back/app/routers/match.router.ts
+++ Back/app/routers/match.router.ts
@@ -2,12 +2,17 @@ import express from 'express'
 import {matchController} from '../controllers/index.controllers'
 
 const MatchRouter = express.Router();
-
+//rout complete /match 
 MatchRouter.post('/', (req, res)=>{
     console.log(req.body.userId);
     matchController.fetchUsersPhotos(req, res);
 });
-//router.post('/authenticate', UserControler);
+
+//rout complete /match/filterUsers
+
+MatchRouter.post('/filterUsers',(req, res)=>{
+    matchController.filterUsers(req);
+})
 
 
 export {MatchRouter}
